0ae11c1ac38076c21595add8932e24baac83f274
nevrohelios <dasharka05@gmail.com>
2025-08-31 22:18:29 +0530

    deploy

--- backend/main.py
+++ backend/main.py
@@ -87,7 +87,3 @@ async def predict(features: LoadFeatures):
     }
 
 
-if __name__ == "__main__":
-    import uvicorn
-    port = os.getenv("PORT", 8000)
-    uvicorn.run(app, host="0.0.0.0", port=int(port))
